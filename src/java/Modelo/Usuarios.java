0dc58c4f3bbd5cc5cb45dbbd55ea20f6183a8187
cesar_ar <cesar_ar@192.168.1.199>
2014-12-02 10:05:39 -0600

    Clase Servlet para iniciar con las peticiones

---
+++ src/java/Modelo/Usuarios.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author cesar_ar
+ */
+public class Usuarios {
+    
+    private String nombre = "";
+    private String pass = "";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+   
+    
+}
